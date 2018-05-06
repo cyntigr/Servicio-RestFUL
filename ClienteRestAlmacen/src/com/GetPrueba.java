66f3c5e16bd638786eac1b6cde112c174651a57e
Cyntia Garcia Ruiz <cyntigr@gmail.com>
2018-05-06 21:18:46 +0200

    Servicio rest

--- ClienteRestAlmacen/src/com/GetPrueba.java
+++ ClienteRestAlmacen/src/com/GetPrueba.java
@@ -4,17 +4,12 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.ClientResponse;
 
-/**
- * @author Crunchify.com
- * 
- */
-
 public class GetPrueba {
 
 	public static void main(String[] args) {
 
 		Client client = Client.create();
-		WebResource webResource = client.resource("http://192.168.0.99:8080/Almacen/rest/almacen/");
+		WebResource webResource = client.resource("http://localhost:8080/Almacenes/rest/almacen/");
 		String s = webResource.accept("application/json").get(String.class);
 		System.out.println(s);
 		ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);  
